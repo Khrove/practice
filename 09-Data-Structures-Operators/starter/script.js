8604f1aeda6ef93714f564d14e45ba0d4b2f3400
Khrove <joshualjohnson33@gmail.com>
2021-01-26 07:58:50 -0500

    Finish Challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -44,38 +44,39 @@ const restaurant = {
   },
 };
 
+
 // Maps: Iteration
-const question = new Map([
-    ['question', 'What is the best programming language in the world?'],
-    [1, 'C'],
-    [2, 'Java'],
-    [3, 'JavaScript'],
-    ['correct', 3],
-    [true, 'Congrats you got the correct answer!'],
-    [false, 'Oof, sorry! You got the wrong answer.']
-]);
-
-console.log(question);
-
-// Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
-
-// Quiz app
-console.log(question.get('question'));
-for(const [key, value] of question) {
-  if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
-}
-const answer = Number(prompt('Your answer?'));
-console.log(answer);
-
-console.log(question.get(question.get('correct') === answer));
-
-// Convert map to array
-console.log([...question]);
-console.log([...question.keys()]);
-console.log([...question.values()]);
+// const question = new Map([
+//     ['question', 'What is the best programming language in the world?'],
+//     [1, 'C'],
+//     [2, 'Java'],
+//     [3, 'JavaScript'],
+//     ['correct', 3],
+//     [true, 'Congrats you got the correct answer!'],
+//     [false, 'Oof, sorry! You got the wrong answer.']
+// ]);
+//
+// console.log(question);
+//
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+//
+// // Quiz app
+// console.log(question.get('question'));
+// for(const [key, value] of question) {
+//   if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = Number(prompt('Your answer?'));
+// console.log(answer);
+//
+// console.log(question.get(question.get('correct') === answer));
+//
+// // Convert map to array
+// console.log([...question]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 
 // Maps
 // Key value pairs where keys can have any type (in objects they're always strings)
