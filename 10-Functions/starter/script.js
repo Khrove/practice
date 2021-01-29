6162b517bac9ffcde36af7d9cd5aa038385ed465
Khrove <joshualjohnson33@gmail.com>
2021-01-29 07:14:34 -0500

    Completed Objects value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,18 +1,40 @@
 'use strict';
+//
+// const bookingArr = [];
+//
+// const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price
+//     }
+//
+//     console.log(booking);
+//     bookingArr.push(booking);
+// }
+//
+// createBooking('LH123');
+// createBooking('AH234', 2, 411);
+// createBooking('BH3433', 2);
 
-const bookingArr = [];
+// Passing objects: Value vs reference
+const flight = 'LH234';
+const jonas = {
+    name: "Joshua Johnson",
+    passport: 23456789
+};
 
-const createBooking = function(flightNum, numPassengers = 1, price = 199 * numPassengers) {
-    const booking = {
-        flightNum,
-        numPassengers,
-        price
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 23456789) {
+        alert('Check in');
+    } else {
+        alert('Wrong passport!');
     }
-
-    console.log(booking);
-    bookingArr.push(booking);
 }
 
-createBooking('LH123');
-createBooking('AH234', 2, 411);
-createBooking('BH3433', 2);
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
