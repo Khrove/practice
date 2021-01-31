9c5dc09d7019889106626604bf5b4d36da68a9d2
Khrove <joshualjohnson33@gmail.com>
2021-01-31 13:59:58 -0500

    Finish HOF, begin functions returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -40,3 +40,39 @@
 // console.log(jonas);
 
 // First class and Higher order functions
+//
+// const oneWord = function(str) {
+//     return str.replace(/ /g, '').toLowerCase();
+// }
+//
+// const upperFirstWord = function(str) {
+//     const [first, ...others] = str.split(' ');
+//     return [first.toUpperCase(), ...others].join(' ');
+// }
+//
+// // Higher order function because it takes in a function as a param
+// const transformer = function(str, fn) {
+//     console.log(`Original string: ${str}`);
+//     console.log(`Transformed string: ${fn(str)}`);
+//
+//     console.log(`Transformed by: ${fn.name}`);
+// }
+//
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+//
+// const highfive = function() {
+//     console.log('Hello!');
+// }
+//
+// document.body.addEventListener('click', highfive);
+
+const greet = function(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
