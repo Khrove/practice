279b1fafae50091375f1d9755ed32ce00a5f1152
Khrove <joshualjohnson33@gmail.com>
2021-02-19 07:41:01 -0500

    Finish flat and flat map

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -348,3 +348,28 @@ btnClose.addEventListener('click', e => {
 // // EVERY
 // console.log(movements.every(mov => mov > 0));
 // console.log(account4.movements.every(mov => mov > 0));
+
+// flat and Flatmap
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flat
+const overallBalanceflat = accounts
+    .map(acc => acc.movements)
+    .flat()
+    .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalanceflat);
+
+// flatMap
+const overallBala
