e157cd8e6e9f8856d012d9d7b452f2d537503445
Khrove <joshualjohnson33@gmail.com>
2021-02-24 07:40:30 -0500

    Continue rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -287,3 +287,20 @@ console.log(Math.max(5, 18, '23px', 11, 2));
 console.log(Math.min(5, 18, 23, 11, 2));
 
 console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(4, 10));
+
+// Rounding Integers
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+// Rounded up -- hit ceiling
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+// Rounding down -- hit floor
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
