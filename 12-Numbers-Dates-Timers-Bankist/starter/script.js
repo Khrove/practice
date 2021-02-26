8207177ea4e09a1eda45dc39ef7c7a7ddea696cb
Khrove <joshualjohnson33@gmail.com>
2021-02-26 07:21:21 -0500

    Finish internationalization

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -357,10 +357,12 @@ btnSort.addEventListener('click', function (e) {
 // console.log(future.getDate());
 
 // Operations with Dates
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(Number(future));
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(Number(future));
+//
+// const dateDiffer = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+//
+// const days1 = dateDiffer(new Date(2037, 3, 14), new Date(2037, 3, 24));
+// console.log(days1);
 
-const dateDiffer = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
-
-const days1 = dateDiffer(new Date(2037, 3, 14), new Date(2037, 3, 24));
-console.log(days1)
+// Internationalization with Dates
